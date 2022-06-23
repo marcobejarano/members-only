997a31f5d3d6fb430e392ba1f32d2d37192ccc6e
Marco <m_uno_p@hotmail.com>
2022-06-23 16:12:35 -0500

    Create post model, controllers and resources in routes file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
   devise_for :users
   resources :users
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
