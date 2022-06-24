c59807c9b584228206752f8cc1136821dc805093
Marco <m_uno_p@hotmail.com>
2022-06-24 13:14:09 -0500

    Add create controller, index view and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.create(post_params)
+    redirect_to root_path
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
